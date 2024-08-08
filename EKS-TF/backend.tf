241c8cced0c4ee6b4808df517e1ef44163488114
Sanjeev Kumar <sanjeevam@spinebiz.com>
2024-08-08 17:15:30 +0530

    changes to dockerhub

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "spine-reddit-bucket-1"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
